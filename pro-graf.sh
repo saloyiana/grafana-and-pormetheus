6f74cab8551f7ae6aa87d03041aea37fbb5b6eea
sara <saronah1099@gmail.com>
2020-09-01 12:37:25 +0300

    changed type

--- pro-graf.sh
+++ pro-graf.sh
@@ -2,7 +2,7 @@
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
 kubectl create namespace monitor
-helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=NodePort
+helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=NodePort --set prometheus.service.type=NodePort
 kubectl apply -f ingress.yaml -n monitor
 sleep 90
 . grafana.sh
